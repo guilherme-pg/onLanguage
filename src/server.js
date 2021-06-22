8770433cce4f62f312a91449bd07caa718dc097e
Guilherme Vieira <gvmmpg@gmail.com>
2021-06-22 15:03:12 -0300

    enabling heroku

--- src/server.js
+++ src/server.js
@@ -12,3 +12,8 @@ app.listen(8080, function(error){
     if(error) throw error 
     console.log("SERVER running on url http://localhost:8080"); 
 });
+
+// package.json
+// "start": "npm-run-all -p nodemon browsersync",
+// 	"nodemon": "nodemon src/server.js",
+// 	"browsersync": "browser-sync start --proxy http://localhost:8080 --file 'public, src/app/views'"
