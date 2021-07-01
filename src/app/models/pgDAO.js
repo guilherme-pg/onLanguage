d5c87f84964dc212f04bdb95e6983436c49e5065
Guilherme Vieira <gvmmpg@gmail.com>
2021-07-01 14:33:43 -0300

    dev query operations

--- src/app/models/pgDAO.js
+++ src/app/models/pgDAO.js
@@ -8,21 +8,42 @@ class WordsDao {
 
 	// RETURN SELECTED DATA
 	read(bodyReqData) {
+		// VARIABLES ANTI-AMBIGUOUS
+		let optionLanguages;
+		let languagesSelectedForTables;
+		let levelsSelectedforTables;
+
+
 		console.log('******* DAO READ');
 		console.log('BBBBBBBBBBBBB bodyReqData ===============>>>>>>>>>   ', bodyReqData);
+
 		let primaryLanguageSelected = bodyReqData.option_language;
-		let levelsSelected = bodyReqData.onption_level;
+		let levelsSelected = bodyReqData.option_level;
+		
 		let themesSelected = bodyReqData.option_theme;
 		let secondLanguageSelected = bodyReqData.option2_language;
+		
+		// DETECTING WITH THERE IS A SECOND LANGUAGE SELECTED FOR A DYNAMIC QUERY
+		if (secondLanguageSelected != undefined) {
+			languagesSelectedForTables = `${primaryLanguageSelected}.words`;
+			optionLanguages = primaryLanguageSelected;
+			levelsSelectedforTables = `${primaryLanguageSelected}.levels`;
+
+		} else {
+			languagesSelectedForTables = `${primaryLanguageSelected}.words, ${secondLanguageSelected}.words`;
+			optionLanguages = `${primaryLanguageSelected}, ${secondLanguageSelected}`;
+			levelsSelectedforTables = `${primaryLanguageSelected}.levels, ${secondLanguageSelected}.levels`;
+		};
+
 
 		return new Promise((resolve, reject) => {
 			pool.connect(function (err, client, done) {
 				if (err) throw err;
 
-				pool.query(`SELECT * FROM ${primaryLanguageSelected}`, (err, res) => {
+				pool.query(`SELECT ${languagesSelectedForTables} FROM ${optionLanguages} WHERE ${levelsSelectedforTables} IN (${levelsSelected.join()}) `, (err, res) => {
 					if (err) throw err;
 					
-					console.log('RES ----->>>    ----->>>>    ', res);
+					console.log('RRRRRRRRRES ----->>>  RES  ----->>>>    ', res);
 
 					let dataset = res;
 
