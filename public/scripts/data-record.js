711466bdc3f459f7209127b1005c9b6d138c8403
Guilherme Vieira <gvmmpg@gmail.com>
2021-07-19 14:38:19 -0300

    tables working and updates

--- public/scripts/data-record.js
+++ public/scripts/data-record.js
@@ -1,9 +1,12 @@
 const optionGrammar = document.getElementsByName('option_grammar');
 const optionGrammarVerb = document.getElementById('option_verb');
 const nounThemes = document.getElementById('noun_themes');
+const optionLanguage = document.getElementsByName('option_language');
+const optionSecondLanguage = document.getElementsByName('option2_language');
+const secondLanguageToHide = document.getElementsByClassName('label_option2_language');
 
 
-
+// change display for grammar option selected
 function switchGrammarDisplay() {
     for (let i = 0; i < optionGrammar.length; i++) {
         if (optionGrammarVerb.checked) {
@@ -13,9 +16,23 @@ function switchGrammarDisplay() {
         };
     };
 };
-
-
-
 for (let i = 0; i < optionGrammar.length; i++) {
     optionGrammar[i].onclick = switchGrammarDisplay;
+};
+
+
+
+// hide same second language when primary language selected
+for (let z = 0; z < optionLanguage.length; z++) {
+    
+    optionLanguage[z].onclick = function () {
+
+        // show all others secondLanguage selected and hide the same as primary selected
+        if (optionLanguage[z].checked) {
+            for (let w = 0; w < secondLanguageToHide.length; w++) {
+                secondLanguageToHide[w].classList.remove('hiddenElement');
+            };
+            secondLanguageToHide[z].classList.add('hiddenElement');
+        };
+    };
 };
