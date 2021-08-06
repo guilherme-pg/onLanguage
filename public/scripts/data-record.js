9ac9ab5b7fe34df8c07c4d84c1d090504545e57f
Guilherme Vieira <gvmmpg@gmail.com>
2021-08-06 14:05:40 -0300

    dynamic language options

--- public/scripts/data-record.js
+++ public/scripts/data-record.js
@@ -4,6 +4,7 @@ const nounThemes = document.getElementById('noun_themes');
 const optionLanguage = document.getElementsByName('option_language');
 const optionSecondLanguage = document.getElementsByName('option2_language');
 const secondLanguageToHide = document.getElementsByClassName('label_option2_language');
+const firstLanguageToHide = document.getElementsByClassName('label_option_language');
 
 
 // change display for grammar option selected
@@ -22,17 +23,28 @@ for (let i = 0; i < optionGrammar.length; i++) {
 
 
 
-// hide same second language when primary language selected
-for (let z = 0; z < optionLanguage.length; z++) {
-    
-    optionLanguage[z].onclick = function () {
-
-        // show all others secondLanguage selected and hide the same as primary selected
+function changeLanguageDisplay() {
+    // show all others secondLanguage selected and hide the same as primary selected
+    for (let z = 0; z < optionLanguage.length; z++) {
         if (optionLanguage[z].checked) {
             for (let w = 0; w < secondLanguageToHide.length; w++) {
                 secondLanguageToHide[w].classList.remove('hiddenElement');
             };
             secondLanguageToHide[z].classList.add('hiddenElement');
         };
+
+        if (optionSecondLanguage[z].checked) {
+            for (let w = 0; w < firstLanguageToHide.length; w++) {
+                firstLanguageToHide[w].classList.remove('hiddenElement');
+            };
+            firstLanguageToHide[z].classList.add('hiddenElement');
+        };
     };
+};
+
+
+// hide same second language when primary language selected
+for (let z = 0; z < optionLanguage.length; z++) {
+    optionLanguage[z].onclick = changeLanguageDisplay;
+    optionSecondLanguage[z].onclick = changeLanguageDisplay;
 };
